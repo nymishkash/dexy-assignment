a9cfdabb506a6637cd83ce337943453c6bf361dc
nymishkash <reachnymish@gmail.com>
2024-12-02 19:20:06 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+
+function App() {
+  return (
+    <div className="flex h-screen flex-col justify-center items-center">
+      Hello World!
+    </div>
+  );
+}
+
+export default App;
